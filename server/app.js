cc8254d23c0397458ef2208f0d84d9057c405f29
Zubayer Hossain <zubayerhossain180@gmail.com>
2026-05-25 19:55:18 +0600

    TRYRTRTRT

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "https://assignment-09-sigma.vercel.app/api/auth/google/callback",
 }, async (accessToken, refreshToken, profile, done) => {
     const coll = db.collection("users");
     let user = await coll.findOne({ email: profile.emails[0].value });
